8bfe89ac8080cd8497e8c2db7cea32db1eca4589
Saharsh Baranwal <saharshbrnwl@gmail.com>
2022-04-13 22:50:10 +0530

    Finally Completed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8002;
+// required db
+const db = require('./config/mongoose');
+
 
 // use express router
 app.use('/', require('./routes'));
@@ -8,6 +11,16 @@ app.use('/', require('./routes'));
 // set up view engine 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+// used static files
+app.use(express.static('./assets'));
+// middleware to read url request
+app.use(express.urlencoded());
+
+// used express routes
+app.use('/',require('./routes/index'));
+
+
 
 app.listen(port, function(err){
     if (err){
